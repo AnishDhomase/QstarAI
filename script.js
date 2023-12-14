b521a30e97b3ab640b492107add9258e57299bdf
AnishDhomase <anishdhomase@gmail.com>
2023-12-14 13:05:36 +0530

    leftBox done with responsiveness

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict"
 
-// ===== Page switch functionality ===============================
+// ===== Pages ===============================
 // pages
 const LandingPage = document.querySelector(".wrapperOne");
 const LoginSignupPage = document.querySelector(".wrapperTwo");
@@ -16,7 +16,18 @@ const LoginSignupPageContinueBtn = document.querySelector(".wrapperTwo .btn butt
 const Fieldset = document.querySelector(".loginSignup .email fieldset");
 const FLegend = document.querySelector(".loginSignup .email fieldset legend");
 const Finput = document.querySelector(".loginSignup .email fieldset input");
+
+// main page
+const ArrowCollapser = document.querySelector(".main .left i.arrow");
+const LeftBox = document.querySelector(".main .left");
+const RightBox = document.querySelector(".main .right");
+const LeftUpBox = document.querySelector(".main .left .upBox");
+const LeftAccBtn = document.querySelector(".main .left button.user");
+const LeftNewBtn = document.querySelector(".main .left button.new");
 // ========================================================================
+
+
+
 
 
 //======== Login page functionality =========================================
@@ -80,4 +91,54 @@ Finput.addEventListener("focusout",()=>{
 
 // ======================================================================
 
+
+// ======= Main page functionality ============================
+// Collapser
+
+ArrowCollapser.addEventListener("mouseover",()=>{
+    if(ArrowCollapser.classList.contains("fa-grip-lines-vertical")){
+        ArrowCollapser.classList.replace("fa-grip-lines-vertical","fa-chevron-left");
+        LeftUpBox.classList.add("darken");
+        LeftAccBtn.classList.add("darken");
+    } 
+})
+ArrowCollapser.addEventListener("mouseout",()=>{
+    if(ArrowCollapser.classList.contains("fa-chevron-left")){
+        ArrowCollapser.classList.replace("fa-chevron-left","fa-grip-lines-vertical");
+        LeftUpBox.classList.remove("darken");
+        LeftAccBtn.classList.remove("darken");
+    }
+})
+ArrowCollapser.addEventListener("click",()=>{
+    if(ArrowCollapser.classList.contains("fa-chevron-left")){
+        if(window.innerWidth <= 450){
+            ArrowCollapser.classList.replace("fa-chevron-left","fa-bars");   
+        }
+        else{
+            ArrowCollapser.classList.replace("fa-chevron-left","fa-chevron-right");
+        }
+        LeftBox.classList.add("translateLeft");
+        LeftNewBtn.classList.add("nohide");
+        LeftUpBox.classList.remove("darken");
+        LeftAccBtn.classList.remove("darken");
+        if(window.innerWidth <= 450){
+            LeftNewBtn.classList.add("mobile");
+        }
+    }
+    else{
+        if(window.innerWidth <= 450){
+            ArrowCollapser.classList.replace("fa-bars","fa-grip-lines-vertical");   
+        }
+        else
+            ArrowCollapser.classList.replace("fa-chevron-right","fa-grip-lines-vertical");
+        LeftBox.classList.remove("translateLeft");
+        LeftNewBtn.classList.remove("nohide");
+        if(window.innerWidth <= 450){
+            LeftNewBtn.classList.remove("mobile");
+        }
+    }
+})
+
+
+// ============================================================
 
